f8f3ab12bb125b934197c51e15c7f4056e0e63f7
iriscarron <iris.carron@edu.esiee.fr>
2025-11-05 11:02:54 +0100

    ok le cleaned_all est bon et le main changé... allez, et test dash ecrit

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from src.utils.clean_caract_2023 import clean_caract
 from src.utils.clean_radars_2023 import clean_radars
 from src.utils.merge_data import merge_cleaned_year
 
+
 # Configuration du logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
+
 
 def main():
     try:
@@ -19,20 +21,24 @@ def main():
         get_radar_2023()
         logger.info("Téléchargement terminé.")
 
+
         # 2. Nettoyage des données
         logger.info("Début du nettoyage des données...")
-        
+       
         # Nettoyage des caractéristiques d'accidents
         logger.info("Nettoyage des données d'accidents...")
         df_caract_clean = clean_caract()
         logger.info(f"Données d'accidents nettoyées: {len(df_caract_clean)} entrées")
+
 
         # Nettoyage des données radar
         logger.info("Nettoyage des données radar...")
         df_radars_clean = clean_radars()
         logger.info(f"Données radar nettoyées: {len(df_radars_clean)} entrées")
 
+
         logger.info("Processus de traitement des données terminé : nettoyage ok.")
+
 
         # 3. Fusion des fichiers nettoyés
         logger.info("Fusion des fichiers nettoyés (tous) ...")
@@ -42,9 +48,11 @@ def main():
         except Exception as me:
             logger.warning(f"La fusion des fichiers nettoyés a échoué : {me}")
 
+
     except Exception as e:
         logger.error(f"Une erreur est survenue: {str(e)}")
         raise
+
 
 if __name__ == "__main__":
     main()
